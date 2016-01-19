97239586076c168966f8253a094283992a3deb0d
Edward Loveall <edward@edwardloveall.com>
2016-01-19 15:35:39 -0500

    Completed SpeakLine

--- SpeakLine/SpeakLine/AppDelegate.swift
+++ SpeakLine/SpeakLine/AppDelegate.swift
@@ -9,21 +9,33 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, NSSpeechSynthesizerDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSSpeechSynthesizerDelegate, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var speakButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
     @IBOutlet weak var sourceText: NSTextField!
+    @IBOutlet weak var voicesTable: NSTableView!
     var speechSynth: NSSpeechSynthesizer?
+    var voices: [String] = []
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    override init() {
+        super.init()
         speechSynth = NSSpeechSynthesizer.init(voice: nil)
         speechSynth?.delegate = self
+        voices = NSSpeechSynthesizer.availableVoices()
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    func applicationDidFinishLaunching(aNotification: NSNotification) {}
+    func applicationWillTerminate(aNotification: NSNotification) {}
+
+    override func awakeFromNib() {
+        let defaultVoice = NSSpeechSynthesizer.defaultVoice()
+        if let defaultRow = voices.indexOf(defaultVoice) {
+            let indices = NSIndexSet(index: defaultRow)
+            voicesTable.selectRowIndexes(indices, byExtendingSelection: false)
+            voicesTable.scrollRowToVisible(defaultRow)
+        }
     }
 
     @IBAction func sayIt(sender: AnyObject) {
@@ -31,6 +43,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSpeechSynthesizerDelegate 
         speechSynth?.startSpeakingString(string)
         stopButton.enabled = true
         speakButton.enabled = false
+        voicesTable.enabled = false
     }
 
     @IBAction func stopIt(sender: AnyObject) {
@@ -40,5 +53,32 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSpeechSynthesizerDelegate 
     func speechSynthesizer(sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
         stopButton.enabled = false
         speakButton.enabled = true
+        voicesTable.enabled = true
+    }
+
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return voices.count
+    }
+
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        let voiceIdentifier = voices[row]
+        let voiceDictionary = NSSpeechSynthesizer.attributesForVoice(voiceIdentifier)
+        return voiceDictionary[NSVoiceName]
+    }
+
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        let row = voicesTable.selectedRow
+        if row == -1 {
+            return
+        }
+        let selectedVoice = voices[row]
+        speechSynth?.setVoice(selectedVoice)
+        print("new voice = \(selectedVoice)")
+    }
+
+    override func respondsToSelector(aSelector: Selector) -> Bool {
+        let methodName = NSStringFromSelector(aSelector)
+        print("responds to \(methodName)")
+        return super.respondsToSelector(aSelector)
     }
 }
