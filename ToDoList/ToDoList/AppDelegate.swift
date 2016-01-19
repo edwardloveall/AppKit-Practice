b990f051349b451289ef980fa978068096f25c43
Edward Loveall <edward@edwardloveall.com>
2016-01-19 16:02:16 -0500

    Refactor todo so it uses a separate delegate class

--- ToDoList/ToDoList/AppDelegate.swift
+++ ToDoList/ToDoList/AppDelegate.swift
@@ -9,41 +9,10 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-    @IBOutlet weak var todoField: NSTextField!
-    @IBOutlet weak var addButton: NSButton!
-    @IBOutlet weak var todoTable: NSTableView!
-    var todoItems: [String]
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {}
     func applicationWillTerminate(aNotification: NSNotification) {}
-
-    override init() {
-        todoItems = [String]()
-    }
-
-    @IBAction func addItem(sender: AnyObject) {
-        let todoString = todoField.stringValue
-        todoItems.append(todoString)
-        todoField.stringValue = ""
-        todoTable.reloadData()
-    }
-
-    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
-        return todoItems.count
-    }
-
-    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
-        return todoItems[row]
-    }
-
-    func tableView(tableView: NSTableView, setObjectValue object: AnyObject?, forTableColumn tableColumn: NSTableColumn?, row: Int) {
-        guard let changedItem = object as? String else {
-            return
-        }
-        todoItems[row] = changedItem
-        todoTable.reloadData()
-    }
 }
