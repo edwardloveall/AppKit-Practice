8e7a4b839828e5a19c15c2793c2ddb1e9806d35c
Edward Loveall <edward@edwardloveall.com>
2016-02-03 14:23:09 -0500

    Complete stubbed interface for RGBWell

--- RGBWell/RGBWell/MainWindowController.swift
+++ RGBWell/RGBWell/MainWindowController.swift
@@ -9,7 +9,21 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+    @IBOutlet weak var colorWell: NSColorWell!
+
     override var windowNibName: String? {
         return "MainWindowController"
+    }
+
+    @IBAction func adjustRed(sender: NSSlider) {
+        print("R slider's value is \(sender.floatValue)")
+    }
+
+    @IBAction func adjustGreen(sender: NSSlider) {
+        print("G slider's value is \(sender.floatValue)")
+    }
+
+    @IBAction func adjustBlue(sender: NSSlider) {
+        print("B slider's value is \(sender.floatValue)")
     }
 }
