fa7ed575ac4e3b55f142c34a9b6c12d80a6ddaa0
Edward Loveall <edward@edwardloveall.com>
2016-02-03 11:55:08 -0500

    Add start of RGBWell project

---
+++ RGBWell/RGBWell/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  RGBWell
+//
+//  Created by Edward Loveall on 2/3/16.
+//  Copyright © 2016 Edward Loveall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var mainWindowController: MainWindowController?
+
+    func applicationDidFinishLaunching(notification: NSNotification) {
+        let mainWindowController = MainWindowController()
+        mainWindowController.showWindow(self)
+        self.mainWindowController = mainWindowController
+    }
+}
---
+++ RGBWell/RGBWell/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Edward Loveall on 2/3/16.
+//  Copyright © 2016 Edward Loveall. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    override var windowNibName: String? {
+        return "MainWindowController"
+    }
+}
