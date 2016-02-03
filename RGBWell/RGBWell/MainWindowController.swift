d36dcca62c12b61d75d7e6da3a040ab3a6488f77
Edward Loveall <edward@edwardloveall.com>
2016-02-03 14:46:56 -0500

    RGBWell working

--- RGBWell/RGBWell/MainWindowController.swift
+++ RGBWell/RGBWell/MainWindowController.swift
@@ -9,6 +9,11 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+    var r = 0.0
+    var g = 0.0
+    var b = 0.0
+    let a = 1.0
+
     @IBOutlet weak var colorWell: NSColorWell!
 
     override var windowNibName: String? {
@@ -16,14 +21,25 @@ class MainWindowController: NSWindowController {
     }
 
     @IBAction func adjustRed(sender: NSSlider) {
-        print("R slider's value is \(sender.floatValue)")
+        r = sender.doubleValue
+        updateColor()
     }
 
     @IBAction func adjustGreen(sender: NSSlider) {
-        print("G slider's value is \(sender.floatValue)")
+        g = sender.doubleValue
+        updateColor()
     }
 
     @IBAction func adjustBlue(sender: NSSlider) {
-        print("B slider's value is \(sender.floatValue)")
+        b = sender.doubleValue
+        updateColor()
+    }
+
+    func updateColor() {
+        let newColor = NSColor(calibratedRed: CGFloat(r),
+                                       green: CGFloat(g),
+                                        blue: CGFloat(b),
+                                       alpha: CGFloat(a))
+        colorWell.color = newColor
     }
 }
