625fe8d0f304417d68f2e006abf80aa31a58ccd7
Edward Loveall <edward@edwardloveall.com>
2016-02-05 09:40:05 -0500

    Busy board basic app window working

---
+++ BusyBoard/BusyBoard/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BusyBoard
+//
+//  Created by Edward Loveall on 2/5/16.
+//  Copyright © 2016 Edward Loveall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var mainWindowController: MainWindowController?
+
+    func applicationDidFinishLaunching(notification: NSNotification) {
+        let mainWindowController = MainWindowController()
+        mainWindowController.showWindow(self)
+        self.mainWindowController = mainWindowController
+    }
+}
+
---
+++ BusyBoard/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  BusyBoard
+//
+//  Created by Edward Loveall on 2/5/16.
+//  Copyright © 2016 Edward Loveall. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+}
