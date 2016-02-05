e0da9a417bfad1f4710a5183e7d86ad88b602207
Edward Loveall <edward@edwardloveall.com>
2016-02-05 09:31:33 -0500

    Pollish up the RGBWell code
    The controls and color well start in the correct state on startup now

--- RGBWell/RGBWell/MainWindowController.swift
+++ RGBWell/RGBWell/MainWindowController.swift
@@ -14,10 +14,22 @@ class MainWindowController: NSWindowController {
     var b = 0.0
     let a = 1.0
 
+    @IBOutlet weak var rSlider: NSSlider?
+    @IBOutlet weak var gSlider: NSSlider!
+    @IBOutlet weak var bSlider: NSSlider!
+
     @IBOutlet weak var colorWell: NSColorWell!
 
     override var windowNibName: String? {
         return "MainWindowController"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        rSlider?.doubleValue = r
+        gSlider.doubleValue = b
+        bSlider.doubleValue = g
+        updateColor()
     }
 
     @IBAction func adjustRed(sender: NSSlider) {
