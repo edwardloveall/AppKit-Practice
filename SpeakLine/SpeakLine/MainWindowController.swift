6350967378d1535469e602c9847e2f08dbe39253
Edward Loveall <edward@edwardloveall.com>
2016-02-16 14:16:03 -0500

    Add voice selection to SpeakLine

--- SpeakLine/SpeakLine/MainWindowController.swift
+++ SpeakLine/SpeakLine/MainWindowController.swift
@@ -8,12 +8,14 @@
 
 import Cocoa
 
-class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate, NSWindowDelegate {
+class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate, NSWindowDelegate, NSTableViewDataSource, NSTableViewDelegate {
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var speakButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var voicesTable: NSTableView!
 
     let speechSynth = NSSpeechSynthesizer()
+    let voices = NSSpeechSynthesizer.availableVoices()
     var isStarted: Bool = false {
         didSet {
             updateButtons()
@@ -28,6 +30,12 @@ class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate, NSW
         super.windowDidLoad()
         updateButtons()
         speechSynth.delegate = self
+        let defaultVoice = NSSpeechSynthesizer.defaultVoice()
+        if let defaultRow = voices.indexOf(defaultVoice) {
+            let indices = NSIndexSet(index: defaultRow)
+            voicesTable.selectRowIndexes(indices, byExtendingSelection: false)
+            voicesTable.scrollRowToVisible(defaultRow)
+        }
     }
 
     // MARK: - Action Methods
@@ -56,16 +64,45 @@ class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate, NSW
         }
     }
 
+    func voiceNameForIdentifier(identifier: String) -> String? {
+        let attributes = NSSpeechSynthesizer.attributesForVoice(identifier)
+        return attributes[NSVoiceName] as? String
+    }
+
     // MARK: - NSSpeechSynthesizerDelegate
 
     func speechSynthesizer(sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
         isStarted = false
-        print("finishedSpeaking=\(finishedSpeaking)")
     }
 
     // MARK: - NSWindowDelegate
 
     func windowShouldClose(sender: AnyObject) -> Bool {
         return !isStarted
+    }
+
+    // MARK: - NSTableViewDataSource
+
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return voices.count
+    }
+
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        let voice = voices[row]
+        let voiceName = voiceNameForIdentifier(voice)
+        return voiceName
+    }
+
+    // MARK: - NSTableViewDelegate
+
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        let row = voicesTable.selectedRow
+        if row == -1 {
+            speechSynth.setVoice(nil)
+            return
+        }
+
+        let voice = voices[row]
+        speechSynth.setVoice(voice)
     }
 }
