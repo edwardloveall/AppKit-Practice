3baf6260673ad4210bcc60c4f208d55f113ba81a
Edward Loveall <edward@edwardloveall.com>
2016-03-03 13:13:26 -0500

    Create PayRaise app for chapter 9

---
+++ PayRaise/PayRaise/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  PayRaise
+//
+//  Created by Edward Loveall on 3/2/16.
+//  Copyright © 2016 Edward Loveall. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
