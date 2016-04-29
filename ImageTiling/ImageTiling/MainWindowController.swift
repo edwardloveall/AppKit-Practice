da42462a510a363801140988c25c35dadd7d9758
Edward Loveall <edward@edwardloveall.com>
2016-04-29 09:58:12 -0400

    Image tiling scroll view example

---
+++ ImageTiling/ImageTiling/MainWindowController.swift
@@ -0,0 +1,21 @@
+//
+//  MainWindowController.swift
+//  ImageTiling
+//
+//  Created by Edward Loveall on 4/27/16.
+//  Copyright © 2016 Edward Loveall. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override var windowNibName: String? {
+        return "MainWindowController"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+
+}
