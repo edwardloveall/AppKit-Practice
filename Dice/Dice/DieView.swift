933fba7b8014328f1f6592ca7fdf6092515cb6c3
Edward Loveall <edward@edwardloveall.com>
2016-04-29 13:16:04 -0400

    Make DiceView @IBDesignable

--- Dice/Dice/DieView.swift
+++ Dice/Dice/DieView.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
-class DieView: NSView {
-    var dotCount: Int? = 3 {
+@IBDesignable class DieView: NSView {
+    @IBInspectable dynamic var dotCount: Int = 1 {
         didSet {
             needsDisplay = true
         }
@@ -34,70 +34,68 @@ class DieView: NSView {
     }
 
     func drawDieWithSize(size: CGSize) {
-        if let dotCount = dotCount {
-            let (edgeLength, dieFrame) = metricsForSize(size)
-            let cornerRadius = edgeLength / 5.0
+        let (edgeLength, dieFrame) = metricsForSize(size)
+        let cornerRadius = edgeLength / 5.0
 
-            let dotRadius = edgeLength / 12.0
-            let dotFrame = dieFrame.insetBy(dx: dotRadius * 2.5,
-                                            dy: dotRadius * 2.5)
+        let dotRadius = edgeLength / 12.0
+        let dotFrame = dieFrame.insetBy(dx: dotRadius * 2.5,
+                                        dy: dotRadius * 2.5)
 
-            NSGraphicsContext.saveGraphicsState()
+        NSGraphicsContext.saveGraphicsState()
 
-            let shadow = NSShadow()
-            shadow.shadowOffset = NSSize(width: 0, height: -1)
-            shadow.shadowBlurRadius = edgeLength / 20
-            shadow.set()
+        let shadow = NSShadow()
+        shadow.shadowOffset = NSSize(width: 0, height: -1)
+        shadow.shadowBlurRadius = edgeLength / 20
+        shadow.set()
 
-            let diePath = NSBezierPath(roundedRect: dieFrame,
-                                       xRadius: cornerRadius,
-                                       yRadius: cornerRadius)
-            NSColor.whiteColor().set()
-            diePath.fill()
+        let diePath = NSBezierPath(roundedRect: dieFrame,
+                                   xRadius: cornerRadius,
+                                   yRadius: cornerRadius)
+        NSColor.whiteColor().set()
+        diePath.fill()
 
-            NSGraphicsContext.restoreGraphicsState()
+        NSGraphicsContext.restoreGraphicsState()
 
+        NSColor.blackColor().set()
+        diePath.lineWidth = edgeLength / 80
+        diePath.stroke()
+
+        func drawDot(horizontalOffset: CGFloat, _ verticalOffset: CGFloat) {
+            let x = dotFrame.minX + dotFrame.width * horizontalOffset
+            let y = dotFrame.minY + dotFrame.height * verticalOffset
+            let dotOrigin = CGPoint(x: x, y: y)
+            let dotRect = CGRect(origin: dotOrigin, size: CGSizeZero)
+                .insetBy(dx: -dotRadius, dy: -dotRadius)
             NSColor.blackColor().set()
-            diePath.lineWidth = edgeLength / 80
-            diePath.stroke()
+            NSBezierPath(ovalInRect: dotRect).fill()
 
-            func drawDot(horizontalOffset: CGFloat, _ verticalOffset: CGFloat) {
-                let x = dotFrame.minX + dotFrame.width * horizontalOffset
-                let y = dotFrame.minY + dotFrame.height * verticalOffset
-                let dotOrigin = CGPoint(x: x, y: y)
-                let dotRect = CGRect(origin: dotOrigin, size: CGSizeZero)
-                    .insetBy(dx: -dotRadius, dy: -dotRadius)
-                NSColor.blackColor().set()
-                NSBezierPath(ovalInRect: dotRect).fill()
+            NSColor(calibratedWhite: 1, alpha: 0.6).set()
+            let glintRadius = dotRadius * 0.2
+            let glintOrigin = CGPoint(x: dotOrigin.x + glintRadius,
+                                      y: dotOrigin.y - glintRadius)
+            let glintRect = CGRect(origin: glintOrigin, size: CGSizeZero)
+                .insetBy(dx: -glintRadius, dy: -glintRadius)
+            NSBezierPath(ovalInRect: glintRect).fill()
+        }
 
-                NSColor(calibratedWhite: 1, alpha: 0.6).set()
-                let glintRadius = dotRadius * 0.2
-                let glintOrigin = CGPoint(x: dotOrigin.x + glintRadius,
-                                          y: dotOrigin.y - glintRadius)
-                let glintRect = CGRect(origin: glintOrigin, size: CGSizeZero)
-                    .insetBy(dx: -glintRadius, dy: -glintRadius)
-                NSBezierPath(ovalInRect: glintRect).fill()
+        if (1...6).indexOf(dotCount) != nil {
+            if dotCount % 2 == 1 {
+                drawDot(0.5, 0.5)
             }
 
-            if (1...6).indexOf(dotCount) != nil {
-                if dotCount % 2 == 1 {
-                    drawDot(0.5, 0.5)
-                }
+            if dotCount > 1 {
+                drawDot(0, 1)
+                drawDot(1, 0)
+            }
 
-                if dotCount > 1 {
-                    drawDot(0, 1)
-                    drawDot(1, 0)
-                }
+            if dotCount > 3 {
+                drawDot(1, 1)
+                drawDot(0, 0)
+            }
 
-                if dotCount > 3 {
-                    drawDot(1, 1)
-                    drawDot(0, 0)
-                }
-
-                if dotCount == 6 {
-                    drawDot(0, 0.5)
-                    drawDot(1, 0.5)
-                }
+            if dotCount == 6 {
+                drawDot(0, 0.5)
+                drawDot(1, 0.5)
             }
         }
     }
