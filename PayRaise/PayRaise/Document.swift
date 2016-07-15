46be9ec05b728a867df056b5eeae362c2e89c78e
Edward Loveall <edward@edwardloveall.com>
2016-07-14 20:48:54 -0400

    Migrate NSLayoutConstraint to NSLayoutAnchor

--- PayRaise/PayRaise/Document.swift
+++ PayRaise/PayRaise/Document.swift
@@ -16,6 +16,8 @@ class Document: NSDocument, NSWindowDelegate {
   @IBOutlet weak var addEmployee: NSButton!
   @IBOutlet weak var removeEmployee: NSButton!
   @IBOutlet weak var view: NSView!
+  let SiblingPadding: CGFloat = 8
+  let ParentPadding: CGFloat = 20
 
     var employees: [Employee] = [] {
         willSet {
@@ -47,91 +49,27 @@ class Document: NSDocument, NSWindowDelegate {
     }
 
     scrollView.translatesAutoresizingMaskIntoConstraints = false
-
-    NSLayoutConstraint(item: addEmployee,
-                       attribute: .Top,
-                       relatedBy: .Equal,
-                       toItem: view,
-                       attribute: .Top,
-                       multiplier: 1,
-                       constant: 20).active = true
-    NSLayoutConstraint(item: addEmployee,
-                       attribute: .Leading,
-                       relatedBy: .Equal,
-                       toItem: scrollView,
-                       attribute: .Trailing,
-                       multiplier: 1,
-                       constant: 8).active = true
-    NSLayoutConstraint(item: removeEmployee,
-                       attribute: .Top,
-                       relatedBy: .Equal,
-                       toItem: addEmployee,
-                       attribute: .Bottom,
-                       multiplier: 1,
-                       constant: 8).active = true
-    NSLayoutConstraint(item: removeEmployee,
-                       attribute: .Leading,
-                       relatedBy: .Equal,
-                       toItem: scrollView,
-                       attribute: .Trailing,
-                       multiplier: 1,
-                       constant: 8).active = true
-    NSLayoutConstraint(item: removeEmployee,
-                       attribute: .Width,
-                       relatedBy: .Equal,
-                       toItem: addEmployee,
-                       attribute: .Width,
-                       multiplier: 1,
-                       constant: 0).active = true
-    NSLayoutConstraint(item: scrollView,
-                       attribute: .Top,
-                       relatedBy: .Equal,
-                       toItem: view,
-                       attribute: .Top,
-                       multiplier: 1,
-                       constant: 20).active = true
-    NSLayoutConstraint(item: scrollView,
-                       attribute: .Leading,
-                       relatedBy: .Equal,
-                       toItem: view,
-                       attribute: .Leading,
-                       multiplier: 1,
-                       constant: 20).active = true
-    NSLayoutConstraint(item: scrollView,
-                       attribute: .Width,
-                       relatedBy: .GreaterThanOrEqual,
-                       toItem: nil,
-                       attribute: .NotAnAttribute,
-                       multiplier: 1,
-                       constant: 200).active = true
-    NSLayoutConstraint(item: scrollView,
-                       attribute: .Height,
-                       relatedBy: .GreaterThanOrEqual,
-                       toItem: nil,
-                       attribute: .NotAnAttribute,
-                       multiplier: 1,
-                       constant: 100).active = true
-    NSLayoutConstraint(item: view,
-                       attribute: .Trailing,
-                       relatedBy: .Equal,
-                       toItem: addEmployee,
-                       attribute: .Trailing,
-                       multiplier: 1,
-                       constant: 20).active = true
-    NSLayoutConstraint(item: view,
-                       attribute: .Bottom,
-                       relatedBy: .Equal,
-                       toItem: scrollView,
-                       attribute: .Bottom,
-                       multiplier: 1,
-                       constant: 20).active = true
-    NSLayoutConstraint(item: view,
-                       attribute: .Bottom,
-                       relatedBy: .GreaterThanOrEqual,
-                       toItem: removeEmployee,
-                       attribute: .Bottom,
-                       multiplier: 1,
-                       constant: 20).active = true
+    addEmployee.topAnchor.constraintEqualToAnchor(view.topAnchor,
+                                                  constant: ParentPadding).active = true
+    addEmployee.leadingAnchor.constraintEqualToAnchor(scrollView.trailingAnchor,
+                                                      constant: SiblingPadding).active = true
+    removeEmployee.topAnchor.constraintEqualToAnchor(addEmployee.bottomAnchor,
+                                                     constant: SiblingPadding).active = true
+    removeEmployee.leadingAnchor.constraintEqualToAnchor(scrollView.trailingAnchor,
+                                                         constant: SiblingPadding).active = true
+    removeEmployee.widthAnchor.constraintEqualToAnchor(addEmployee.widthAnchor).active = true
+    scrollView.topAnchor.constraintEqualToAnchor(view.topAnchor,
+                                                 constant: ParentPadding).active = true
+    scrollView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor,
+                                                     constant: ParentPadding).active = true
+    scrollView.widthAnchor.constraintGreaterThanOrEqualToConstant(200).active = true
+    scrollView.heightAnchor.constraintGreaterThanOrEqualToConstant(100).active = true
+    view.trailingAnchor.constraintEqualToAnchor(addEmployee.trailingAnchor,
+                                                constant: ParentPadding).active = true
+    view.bottomAnchor.constraintEqualToAnchor(scrollView.bottomAnchor,
+                                              constant: ParentPadding).active = true
+    view.bottomAnchor.constraintGreaterThanOrEqualToAnchor(removeEmployee.bottomAnchor,
+                                                           constant: ParentPadding).active = true
   }
 
     override class func autosavesInPlace() -> Bool {
